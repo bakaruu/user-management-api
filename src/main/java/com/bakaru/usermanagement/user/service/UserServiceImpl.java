914c517990802e738bf0f69720cc400e28b34329
bakaruu <arucastro@gmail.com>
2026-09-09 01:54:18 +0200

    Fix LazyInitializationException on refresh token by wrapping in a transaction

--- src/main/java/com/bakaru/usermanagement/user/service/UserServiceImpl.java
+++ src/main/java/com/bakaru/usermanagement/user/service/UserServiceImpl.java
@@ -14,6 +14,7 @@ import com.bakaru.usermanagement.security.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
@@ -84,6 +85,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public AuthResponse refreshToken(RefreshTokenRequest request) {
         User user = refreshTokenService.validateAndGetUser(request.getRefreshToken());
 
